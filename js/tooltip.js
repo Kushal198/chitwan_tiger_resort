b9df5a44deaf3a3f4adcc2954605d16a10b9abc1
kushal198 <kushalkc198@gmail.com>
2022-05-03 17:14:39 +0545

    minor

--- js/tooltip.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    setTimeout(function () {
-        document.getElementById('toolTip').style.visibility = 'hidden';
-    }, 8000);
-}, false);
-
